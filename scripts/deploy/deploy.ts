a98be01feaa72c0b656996d9eb95b335ccfb7434
ultron <software@ultron.foundation>
2022-06-25 12:11:16 +0300

    update(Sergej): deploy script, tsconfig

--- scripts/deploy/deploy.ts
+++ scripts/deploy/deploy.ts
@@ -1,22 +1,38 @@
 import { subtask, task, types } from "hardhat/config";
 
 task("deploy", "Deploy")
-  .addParam("setter", "feeToSetter Factory", "", types.string)
-  .addParam("factory", "Factory address", "", types.string)
-  .addParam("wulx", "wULX address", "", types.string)
-  .setAction(async (taskArgs, {ethers}) => {
-        let FactoryAddress = taskArgs.factory
-        if (FactoryAddress === "" && taskArgs.setter !== "") {
-              const Factory = await ethers.getContractFactory("UniswapV2Factory");
-              const factory = await Factory.deploy(taskArgs.setter);
-              await factory.deployed();
-              FactoryAddress = factory.address
-              console.log("Factory deployed to:", FactoryAddress);
-          }
-        if (taskArgs.wulx !== "" && FactoryAddress !== "") {
-              const Router = await ethers.getContractFactory("UniswapV2Router02");
-              const router = await Router.deploy(FactoryAddress, taskArgs.wulx);
-              await router.deployed();
-              console.log("Router deployed to:", router.address);
-        }
+  .setAction(async (taskArgs, {run, ethers, network}) => {
+      const uniswapV2Factory = await run("UniswapV2Factory");
+
+      const weth = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"; // goerli
+      const uniswapV2Router = await run("UniswapV2Router", { factory: uniswapV2Factory, weth: weth });
+
+      console.log("=".repeat(50));
+      console.log(`UniswapV2Factory: \x1b[32m$${uniswapV2Factory}\x1b[0m`);
+      console.log(`UniswapV2Router: \x1b[32m$${uniswapV2Router}\x1b[0m`);
   });
+
+/*========== UniswapV2Factory ==========*/
+subtask("UniswapV2Factory", "The contract UniswapV2Factory is deployed")
+      .setAction(async (_, { ethers, network }) => {
+            const signer = (await ethers.getSigners())[0];
+            const feeToSetter = signer.address;
+
+            const UniswapV2Factory_Factory = await ethers.getContractFactory("UniswapV2Factory", signer);
+            const UniswapV2Factory = await (await UniswapV2Factory_Factory.deploy(feeToSetter)).deployed();
+            console.log(`The UniswapV2Factory: \u001b[1;34m${UniswapV2Factory.address}\u001b[0m`);    
+            return UniswapV2Factory.address;
+      });
+
+/*========== UniswapV2Router ==========*/
+subtask("UniswapV2Router", "The contract UniswapV2Router is deployed")
+      .addParam("factory", "UniswapV2Factory address", "", types.string)
+      .addParam("weth", "wETH address", "", types.string)
+      .setAction(async (taskArgs, { ethers, network }) => {
+            const signer = (await ethers.getSigners())[0];
+
+            const UniswapV2RouterFactory = await ethers.getContractFactory("UniswapV2Router", signer);
+            const UniswapV2Router = await (await UniswapV2RouterFactory.deploy(taskArgs.factory, taskArgs.weth)).deployed();
+            console.log(`The UniswapV2Router: \u001b[1;34m${UniswapV2Router.address}\u001b[0m`);    
+            return UniswapV2Router.address;
+      });
