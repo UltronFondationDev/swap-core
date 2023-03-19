45a32d0ffeed3428d3746a7d6ca98fa0f36a6540
ultron <software@ultron.foundation>
2023-03-19 20:25:24 +0100

    Make improvements to task to verify code

--- deploy/verify-code.ts
+++ deploy/verify-code.ts
@@ -1,12 +1,14 @@
 import fs from 'node:fs/promises'
 import { task, types } from 'hardhat/config'
 import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names'
+import { HttpNetworkUserConfig, NetworkConfig } from 'hardhat/types'
 
 task('verify-code', 'Compare code for UniswapV2Factory contract')
   .addParam<string>('contractName', 'Contract name', undefined, types.string)
   .addParam<string>('contractAddress', 'Contract address', undefined, types.string)
-  .addOptionalParam<string>('folder', 'Folder', undefined, types.string)
-  .setAction(async ({ contractAddress, contractName, folder }, { ethers, run, network, config }) => {
+  .addOptionalParam<string>('folder', 'Contract folder', undefined, types.string)
+  .addOptionalParam<string>('lpPair', 'Liquidity pool pair', undefined, types.string)
+  .setAction(async ({ contractAddress, contractName, folder, lpPair }, { ethers, run, network, config }) => {
     await run(TASK_COMPILE)
 
     const { compilers } = config.solidity
@@ -18,12 +20,18 @@ task('verify-code', 'Compare code for UniswapV2Factory contract')
       settings: { optimizer },
     } = compilers[0]
 
+    const networkConfig = network.config as HttpNetworkUserConfig
     console.log(`Network: ${network.name}`)
-    console.log(`Solidity compiler version: ${version}`)
+    console.log(`Chain id: ${networkConfig.chainId}`)
+    console.log(`Provider URL: ${networkConfig.url}`)
+    console.log(`\nContract name: ${contractName}`)
+    console.log(`Contract address: ${contractAddress}`)
+    if (lpPair) {
+      console.log(`Liquidity pool pair: ${lpPair}`)
+    }
+    console.log(`\nSolidity compiler version: ${version}`)
     console.log(`Solidity compiler optimizer enabled: ${optimizer.enabled}`)
     console.log(`Solidity compiler optimizer runs: ${optimizer.runs}`)
-    console.log(`Contract name: ${contractName}`)
-    console.log(`Contract address: ${contractAddress}`)
 
     const artifact = await fs.readFile(
       `./artifacts/contracts/${folder ? `${folder}/` : '/'}${contractName}.sol/${contractName}.json`
@@ -35,17 +43,17 @@ task('verify-code', 'Compare code for UniswapV2Factory contract')
       throw new Error(`Build code size doesn't match chain code size`)
     }
 
-    console.log(`Build code matches chain code by ${calculateSimilarityRatio(buildCode, chainCode, 4)}%`)
+    console.log(`\nBuild code matches chain code by ${calculateSimilarityRatio(buildCode, chainCode, 4)}%`)
 
     await fs.rm('./build', { recursive: true, force: true })
     await fs.mkdir('./build')
 
-    const buildCodeFilePath = `./build/build-code-${contractName}.txt`
+    const buildCodeFilePath = `./build/build-code-${contractName}.bin-runtime`
     await fs.writeFile(buildCodeFilePath, buildCode)
     console.log(`Build code saved to ${buildCodeFilePath}`)
 
-    const chainCodeFilePath = `./build/chain-code-${contractName}.txt`
-    await fs.writeFile(`./build/chain-code-${contractAddress}.txt`, chainCode)
+    const chainCodeFilePath = `./build/chain-code-${contractName}.bin-runtime`
+    await fs.writeFile(chainCodeFilePath, chainCode)
     console.log(`Chain code saved to ${chainCodeFilePath}`)
   })
 
